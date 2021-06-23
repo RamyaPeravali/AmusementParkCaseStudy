81ac5c3e2fb93666c5750d18150841dc3e8f7d3c
Ramya Peravali <ramyaperavali136@gmail.com>
2021-06-23 20:44:15 +0530

    Request path changed

--- src/main/java/com/cg/mts/web/ActivityController.java
+++ src/main/java/com/cg/mts/web/ActivityController.java
@@ -3,6 +3,7 @@ package com.cg.mts.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.cg.mts.dto.SuccessMessageDto;
 import com.cg.mts.entities.Activity;
 import com.cg.mts.util.ActivityConstants;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/activity")
 public class ActivityController {
